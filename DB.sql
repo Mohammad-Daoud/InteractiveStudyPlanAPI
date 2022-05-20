9c56a98f2274315c9a5db8434d5f3e3bd371edf4
Mohammad Daoud <m.a.daoud99@gmail.com>
2022-05-20 15:24:00 +0300

    update db

--- DB.sql
+++ DB.sql
@@ -21,15 +21,17 @@ drop table if exists Student;
 CREATE TABLE Student (
     fname VARCHAR(100) NOT NULL,
     lname VARCHAR(100) NOT NULL,
-    username VARCHAR(100) NOT NULL UNIQUE,
+    username VARCHAR(100) NOT NULL ,
     password VARCHAR(255) NOT NULL,
-    schoolName VARCHAR(100) NOT NULL UNIQUE,
-    depName VARCHAR(100) NOT NULL UNIQUE,
+    schoolName VARCHAR(100) NOT NULL ,
+    depName VARCHAR(100) NOT NULL ,
     planYear INT(5) NOT NULL,
     userType VARCHAR(10) DEFAULT 'STUDENT',
     PRIMARY KEY (username)
 );
 insert into Student(fname, lname, username, password, schoolName, depName, planYear) values ('Mohammad', 'Daoud','mhm0173632','mhm0173632','kasit','cs', 2017);
+insert into Student(fname, lname, username, password, schoolName, depName, planYear) values ('Husam', 'Jbr','hsa0189136','hsa0189136','kasit','cis', 2018);
+insert into Student(fname, lname, username, password, schoolName, depName, planYear) values ('Hamza', 'Amerah','hmz0189136','hmz0189136','kasit','cs', 2018);
 
 drop table if exists Instructor;
 CREATE TABLE Instructor (
@@ -149,3 +151,171 @@ CREATE TABLE Takes (
         REFERENCES Student (username)
 );
 
+-- INSERT INTO Takes values (1900150,'mhm0173632',0);
+-- INSERT INTO Takes values (1901234,'mhm0173632',1);
+-- INSERT INTO Takes values (1901233,'mhm0173632',0);
+-- INSERT INTO Takes values (3202098,'mhm0173632',0);
+-- INSERT INTO Takes values (1901496,'mhm0173632',0);
+-- INSERT INTO Takes values (1901442,'mhm0173632',1);
+-- INSERT INTO Takes values (1901376,'mhm0173632',0);
+-- INSERT INTO Takes values (1931102,'mhm0173632',1);
+-- INSERT INTO Takes values (1901466,'mhm0173632',1);
+-- INSERT INTO Takes values (1901241,'mhm0173632',1);
+-- INSERT INTO Takes values (1901471,'mhm0173632',0);
+-- INSERT INTO Takes values (1901237,'mhm0173632',0);
+-- INSERT INTO Takes values (1901498,'mhm0173632',0);
+-- INSERT INTO Takes values (1904121,'mhm0173632',0);
+-- INSERT INTO Takes values (3202100,'mhm0173632',0);
+-- INSERT INTO Takes values (300102,'mhm0173632',0);
+-- INSERT INTO Takes values (1901101,'mhm0173632',0);
+-- INSERT INTO Takes values (302116,'mhm0173632',0);
+-- INSERT INTO Takes values (1932099,'mhm0173632',0);
+-- INSERT INTO Takes values (301131,'mhm0173632',1);
+-- INSERT INTO Takes values (1901494,'mhm0173632',0);
+-- INSERT INTO Takes values (1901464,'mhm0173632',1);
+-- INSERT INTO Takes values (1901475,'mhm0173632',0);
+-- INSERT INTO Takes values (3400105,'mhm0173632',1);
+-- INSERT INTO Takes values (1902454,'mhm0173632',1);
+-- INSERT INTO Takes values (1902098,'mhm0173632',1);
+-- INSERT INTO Takes values (302108,'mhm0173632',0);
+-- INSERT INTO Takes values (1904353,'mhm0173632',0);
+-- INSERT INTO Takes values (1902223,'mhm0173632',1);
+-- INSERT INTO Takes values (720100,'mhm0173632',0);
+-- INSERT INTO Takes values (3202099,'mhm0173632',1);
+-- INSERT INTO Takes values (1901497,'mhm0173632',1);
+-- INSERT INTO Takes values (1901204,'mhm0173632',1);
+-- INSERT INTO Takes values (1901463,'mhm0173632',1);
+-- INSERT INTO Takes values (3400106,'mhm0173632',0);
+-- INSERT INTO Takes values (1901322,'mhm0173632',1);
+-- INSERT INTO Takes values (301101,'mhm0173632',0);
+-- INSERT INTO Takes values (1901353,'mhm0173632',0);
+-- INSERT INTO Takes values (1901473,'mhm0173632',1);
+-- INSERT INTO Takes values (3201099,'mhm0173632',1);
+-- INSERT INTO Takes values (1901472,'mhm0173632',1);
+-- INSERT INTO Takes values (400102,'mhm0173632',1);
+-- INSERT INTO Takes values (1904101,'mhm0173632',0);
+-- INSERT INTO Takes values (1904341,'mhm0173632',1);
+-- INSERT INTO Takes values (3400101,'mhm0173632',1);
+-- INSERT INTO Takes values (1902474,'mhm0173632',1);
+-- INSERT INTO Takes values (3400104,'mhm0173632',0);
+-- INSERT INTO Takes values (1901469,'mhm0173632',0);
+-- INSERT INTO Takes values (2300101,'mhm0173632',0);
+-- INSERT INTO Takes values (1902353,'mhm0173632',1);
+-- INSERT INTO Takes values (3400103,'mhm0173632',0);
+-- INSERT INTO Takes values (1902371,'mhm0173632',1);
+-- INSERT INTO Takes values (1902202,'mhm0173632',0);
+-- INSERT INTO Takes values (3400108,'mhm0173632',0);
+-- INSERT INTO Takes values (1901446,'mhm0173632',1);
+-- INSERT INTO Takes values (3201100,'mhm0173632',1);
+-- INSERT INTO Takes values (1901362,'mhm0173632',1);
+-- INSERT INTO Takes values (3400109,'mhm0173632',0);
+-- INSERT INTO Takes values (1901238,'mhm0173632',0);
+-- INSERT INTO Takes values (2200103,'mhm0173632',1);
+-- INSERT INTO Takes values (1901359,'mhm0173632',1);
+-- INSERT INTO Takes values (3201098,'mhm0173632',1);
+-- INSERT INTO Takes values (3400107,'mhm0173632',0);
+-- INSERT INTO Takes values (3400100,'mhm0173632',1);
+-- INSERT INTO Takes values (1600100,'mhm0173632',1);
+-- INSERT INTO Takes values (1901457,'mhm0173632',0);
+-- INSERT INTO Takes values (1901443,'mhm0173632',0);
+-- INSERT INTO Takes values (3400102,'mhm0173632',0);
+-- INSERT INTO Takes values (1902342,'mhm0173632',0);
+-- INSERT INTO Takes values (2300102,'mhm0173632',0);
+-- INSERT INTO Takes values (1901467,'mhm0173632',1);
+-- INSERT INTO Takes values (1100100,'mhm0173632',1);
+-- INSERT INTO Takes values (1601105,'mhm0173632',0);
+-- INSERT INTO Takes values (1901459,'mhm0173632',1);
+-- INSERT INTO Takes values (1901341,'mhm0173632',0);
+-- INSERT INTO Takes values (1901461,'mhm0173632',0);
+-- INSERT INTO Takes values (400101,'mhm0173632',1);
+-- INSERT INTO Takes values (1900101,'mhm0173632',0);
+-- INSERT INTO Takes values (2220100,'mhm0173632',1);
+-- INSERT INTO Takes values (1901351,'mhm0173632',1);
+-- INSERT INTO Takes values (1901452,'mhm0173632',0);
+-- INSERT INTO Takes values (2000100,'mhm0173632',1);
+-- INSERT INTO Takes values (1000102,'mhm0173632',0);
+
+
+-- INSERT INTO Takes values (1901442,'hsa0189136',1);
+-- INSERT INTO Takes values (1901233,'hsa0189136',0);
+-- INSERT INTO Takes values (1901464,'hsa0189136',1);
+-- INSERT INTO Takes values (3400106,'hsa0189136',1);
+-- INSERT INTO Takes values (1901494,'hsa0189136',0);
+-- INSERT INTO Takes values (3202098,'hsa0189136',1);
+-- INSERT INTO Takes values (1901234,'hsa0189136',0);
+-- INSERT INTO Takes values (301131,'hsa0189136',0);
+-- INSERT INTO Takes values (1901475,'hsa0189136',0);
+-- INSERT INTO Takes values (1901101,'hsa0189136',0);
+-- INSERT INTO Takes values (1901353,'hsa0189136',0);
+-- INSERT INTO Takes values (300102,'hsa0189136',1);
+-- INSERT INTO Takes values (1901362,'hsa0189136',0);
+-- INSERT INTO Takes values (3202100,'hsa0189136',1);
+-- INSERT INTO Takes values (1902223,'hsa0189136',0);
+-- INSERT INTO Takes values (1901376,'hsa0189136',1);
+-- INSERT INTO Takes values (1901237,'hsa0189136',1);
+-- INSERT INTO Takes values (1901466,'hsa0189136',0);
+-- INSERT INTO Takes values (1902454,'hsa0189136',0);
+-- INSERT INTO Takes values (302116,'hsa0189136',1);
+-- INSERT INTO Takes values (1901469,'hsa0189136',0);
+-- INSERT INTO Takes values (301101,'hsa0189136',0);
+-- INSERT INTO Takes values (1901496,'hsa0189136',1);
+-- INSERT INTO Takes values (1900150,'hsa0189136',0);
+-- INSERT INTO Takes values (3400109,'hsa0189136',0);
+-- INSERT INTO Takes values (1901467,'hsa0189136',1);
+-- INSERT INTO Takes values (2200103,'hsa0189136',0);
+-- INSERT INTO Takes values (302108,'hsa0189136',0);
+-- INSERT INTO Takes values (1902474,'hsa0189136',1);
+-- INSERT INTO Takes values (1901471,'hsa0189136',1);
+-- INSERT INTO Takes values (1901204,'hsa0189136',1);
+-- INSERT INTO Takes values (1904101,'hsa0189136',0);
+-- INSERT INTO Takes values (1901241,'hsa0189136',1);
+-- INSERT INTO Takes values (3202099,'hsa0189136',0);
+-- INSERT INTO Takes values (1901359,'hsa0189136',1);
+-- INSERT INTO Takes values (720100,'hsa0189136',1);
+-- INSERT INTO Takes values (1901351,'hsa0189136',0);
+-- INSERT INTO Takes values (400102,'hsa0189136',0);
+-- INSERT INTO Takes values (3201099,'hsa0189136',1);
+-- INSERT INTO Takes values (3400108,'hsa0189136',1);
+-- INSERT INTO Takes values (1901322,'hsa0189136',1);
+-- INSERT INTO Takes values (1902202,'hsa0189136',1);
+-- INSERT INTO Takes values (1901463,'hsa0189136',0);
+-- INSERT INTO Takes values (1902353,'hsa0189136',0);
+-- INSERT INTO Takes values (1931102,'hsa0189136',0);
+-- INSERT INTO Takes values (2000100,'hsa0189136',1);
+-- INSERT INTO Takes values (1901461,'hsa0189136',0);
+-- INSERT INTO Takes values (1902371,'hsa0189136',0);
+-- INSERT INTO Takes values (1901498,'hsa0189136',1);
+-- INSERT INTO Takes values (1932099,'hsa0189136',0);
+-- INSERT INTO Takes values (1901497,'hsa0189136',0);
+-- INSERT INTO Takes values (1901443,'hsa0189136',1);
+-- INSERT INTO Takes values (1901459,'hsa0189136',1);
+-- INSERT INTO Takes values (1904121,'hsa0189136',1);
+-- INSERT INTO Takes values (1904353,'hsa0189136',1);
+-- INSERT INTO Takes values (1901473,'hsa0189136',1);
+-- INSERT INTO Takes values (1901446,'hsa0189136',0);
+-- INSERT INTO Takes values (1901238,'hsa0189136',0);
+-- INSERT INTO Takes values (3400107,'hsa0189136',1);
+-- INSERT INTO Takes values (3201100,'hsa0189136',1);
+-- INSERT INTO Takes values (1600100,'hsa0189136',0);
+-- INSERT INTO Takes values (1901341,'hsa0189136',0);
+-- INSERT INTO Takes values (1900101,'hsa0189136',1);
+-- INSERT INTO Takes values (3400101,'hsa0189136',1);
+-- INSERT INTO Takes values (1901452,'hsa0189136',0);
+-- INSERT INTO Takes values (1901457,'hsa0189136',0);
+-- INSERT INTO Takes values (2220100,'hsa0189136',0);
+-- INSERT INTO Takes values (1901472,'hsa0189136',1);
+-- INSERT INTO Takes values (3400105,'hsa0189136',1);
+-- INSERT INTO Takes values (3201098,'hsa0189136',0);
+-- INSERT INTO Takes values (1904341,'hsa0189136',0);
+-- INSERT INTO Takes values (1902342,'hsa0189136',0);
+-- INSERT INTO Takes values (1902098,'hsa0189136',1);
+-- INSERT INTO Takes values (2300102,'hsa0189136',0);
+-- INSERT INTO Takes values (400101,'hsa0189136',0);
+-- INSERT INTO Takes values (1000102,'hsa0189136',0);
+-- INSERT INTO Takes values (1100100,'hsa0189136',1);
+-- INSERT INTO Takes values (3400104,'hsa0189136',0);
+-- INSERT INTO Takes values (2300101,'hsa0189136',0);
+-- INSERT INTO Takes values (3400102,'hsa0189136',0);
+-- INSERT INTO Takes values (3400103,'hsa0189136',1);
+-- INSERT INTO Takes values (1601105,'hsa0189136',0);
+-- INSERT INTO Takes values (3400100,'hsa0189136',0);
